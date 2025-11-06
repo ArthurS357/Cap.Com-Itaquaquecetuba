0b1d043c4e061d59a05ce2bd831b335449ab72de
Arthur S. <arthursabino5342@gmail.com>
2025-11-06 15:26:01 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,21 +9,19 @@ export default function App({ Component, pageProps }: AppProps) {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Define um timer para remover a tela de splash
-    // Tempo reduzido para 700ms para carregamento mais rápido
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 700); // 0.7 segundos
 
     return () => clearTimeout(timer);
-  }, []); // O array vazio [] garante que isso rode apenas uma vez 
+  }, []); 
 
   if (isLoading) {
     return <WelcomeSplash />;
   }
 
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark">
+    <ThemeProvider attribute="class" defaultTheme="system">
       <Layout>
         <Component {...pageProps} />
       </Layout>
