b6fdcf9a8325e6fa5d1575a1224cd77fae002a67
Arthur S. <arthursabino5342@gmail.com>
2025-11-18 08:12:46 -0300

    Update uploadthing.ts

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -1,15 +1,11 @@
-import { createUploadthing, type FileRouter } from "uploadthing/next-legacy";
+import { createUploadthing, type FileRouter } from "uploadthing/server";
 
 const f = createUploadthing();
 
-// FileRouter para a sua aplicação
 export const ourFileRouter = {
-  // Define uma rota chamada "imageUploader"
   imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
-    .onUploadComplete(async ({ metadata, file }) => {
-      // Este código roda no servidor após o upload
+    .onUploadComplete(async ({ file }) => {
       console.log("Upload completo para a URL:", file.url);
-      
       return { url: file.url };
     }),
 } satisfies FileRouter;
