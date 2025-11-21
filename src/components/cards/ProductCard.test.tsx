d4c7429f7e59690b2acd41899913a53d03777f6a
Arthur S. <arthursabino5342@gmail.com>
2025-11-21 12:48:42 -0300

    feat: Removido afterEach, beforeEach

--- src/components/cards/ProductCard.test.tsx
+++ src/components/cards/ProductCard.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi, afterEach, beforeEach } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import ProductCard, { type MinimalProduct } from './ProductCard'; 
 
