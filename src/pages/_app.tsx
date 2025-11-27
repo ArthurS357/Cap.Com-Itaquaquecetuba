31dcac667955df5f351d6a820af157e2965b0d10
Arthur S. <arthursabino5342@gmail.com>
2025-11-27 12:51:54 -0300

    refactor: wrap application with cart provider and add CartSidebar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import WelcomeSplash from '@/components/WelcomeSplash';
 import { ThemeProvider } from 'next-themes';
 import { SessionProvider } from "next-auth/react"; 
 import { Toaster } from 'react-hot-toast';
+import { CartProvider } from '@/context/CartContext'; 
+import CartSidebar from '@/components/CartSidebar';   
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [showSplash, setShowSplash] = useState(true);
@@ -27,18 +29,25 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
 
   return (
     <SessionProvider session={session}>
-      {showSplash && (
-        <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-surface-background">
-           <WelcomeSplash />
-        </div>
-      )}
+      {/* Envolve a aplicação com o CartProvider para o estado ser global */}
+      <CartProvider>
+        
+        {showSplash && (
+          <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-surface-background">
+             <WelcomeSplash />
+          </div>
+        )}
 
-      <ThemeProvider attribute="class" defaultTheme="system">
-        <Layout>
-          <Component {...pageProps} />
-          <Toaster position="top-center" reverseOrder={false} />
-        </Layout>
-      </ThemeProvider>
+        <ThemeProvider attribute="class" defaultTheme="system">
+          <Layout>
+            <Component {...pageProps} />
+            <Toaster position="top-center" reverseOrder={false} />
+            {/* O componente visual do carrinho fica disponível em todas as páginas */}
+            <CartSidebar /> 
+          </Layout>
+        </ThemeProvider>
+
+      </CartProvider>
     </SessionProvider>
   );
 }
