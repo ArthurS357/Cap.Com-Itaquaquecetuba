342574b14a46a7d14a561ed337b14bbd3d50ee92
Arthur S. <arthursabino5342@gmail.com>
2025-11-28 11:32:31 -0300

    feat: wrap application with WishlistProvider to manage favorites

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,10 +8,12 @@ import { SessionProvider } from "next-auth/react";
 import { Toaster } from 'react-hot-toast';
 import { CartProvider } from '@/context/CartContext'; 
 import CartSidebar from '@/components/CartSidebar';   
+import { WishlistProvider } from '@/context/WishlistContext'; // Importação da Wishlist
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [showSplash, setShowSplash] = useState(true);
 
+  // Lógica da Tela de Boas-vindas (Splash Screen)
   useEffect(() => {
     const hasVisited = localStorage.getItem('has_visited_capcom');
 
@@ -29,25 +31,26 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
 
   return (
     <SessionProvider session={session}>
-      {/* Envolve a aplicação com o CartProvider para o estado ser global */}
-      <CartProvider>
-        
-        {showSplash && (
-          <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-surface-background">
-             <WelcomeSplash />
-          </div>
-        )}
+      <WishlistProvider>
+        <CartProvider>
+          
+          {showSplash && (
+            <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-surface-background">
+               <WelcomeSplash />
+            </div>
+          )}
 
-        <ThemeProvider attribute="class" defaultTheme="system">
-          <Layout>
-            <Component {...pageProps} />
-            <Toaster position="top-center" reverseOrder={false} />
-            {/* O componente visual do carrinho fica disponível em todas as páginas */}
-            <CartSidebar /> 
-          </Layout>
-        </ThemeProvider>
+          <ThemeProvider attribute="class" defaultTheme="system">
+            <Layout>
+              <Component {...pageProps} />
+              <Toaster position="top-center" reverseOrder={false} />
+              {/* O Carrinho Lateral fica disponível globalmente */}
+              <CartSidebar /> 
+            </Layout>
+          </ThemeProvider>
 
-      </CartProvider>
+        </CartProvider>
+      </WishlistProvider>
     </SessionProvider>
   );
 }
